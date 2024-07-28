012f6fd7d81f44397e132403f4a04c1f8a5967ea
Mircea Dobreanu <mircea@dobreanu.net>
2024-07-28 19:11:39 +0300

    Add doc to Employee example for decorators

--- src/main/java/me/mircea/patterns/design/structural/decorator/Employee.java
+++ src/main/java/me/mircea/patterns/design/structural/decorator/Employee.java
@@ -1,5 +1,11 @@
 package me.mircea.patterns.design.structural.decorator;
 
+/**
+ * Examples of decorators:
+ * <p>
+ * - in Java SE API: java.io.Reader and java.io.Writer with classes
+ * such as BufferedReader, BufferedWriter, PrintWriter, etc.
+ */
 public interface Employee {
     int computeSalary();
 }
