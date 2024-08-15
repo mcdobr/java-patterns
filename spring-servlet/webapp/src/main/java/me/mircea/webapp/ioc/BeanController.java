b2b7bf3d4fdf0b653f5dd28d984b73870d26b12b
Mircea Dobreanu <mircea@dobreanu.net>
2024-08-15 14:03:33 +0300

    Use different methods for injecting shorter lived beans in longer lived ones

--- spring-servlet/webapp/src/main/java/me/mircea/webapp/ioc/BeanController.java
+++ spring-servlet/webapp/src/main/java/me/mircea/webapp/ioc/BeanController.java
@@ -6,11 +6,22 @@ import me.mircea.webapp.ioc.prototype.PrototypeBean;
 import me.mircea.webapp.ioc.request.RequestBean;
 import me.mircea.webapp.ioc.session.SessionBean;
 import me.mircea.webapp.ioc.singleton.SingletonBean;
+import org.springframework.beans.factory.ObjectProvider;
 import org.springframework.context.ApplicationContext;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
+/**
+ * There are multiple ways to inject a shorter-lived bean into a longer-lived bean.
+ * This is an example of a singleton controller.
+ * <p>
+ * Ways you can inject:
+ * - use the ApplicationContext and get a bean of a specific class
+ * - inject an ObjectFactory of that type from Spring
+ * - inject an ObjectProvider of that type from Spring (specialization of ObjectFactory)
+ * - Add JSR 330 dependency and use Provider interface
+ */
 @RestController
 @RequestMapping("/api/beans")
 @RequiredArgsConstructor
@@ -19,6 +30,8 @@ public class BeanController {
 
     private final SingletonBean singletonBean;
 
+    private final ObjectProvider<PrototypeBean> prototypeBeanObjectProvider;
+
     @GetMapping("/singleton")
     public int getSingleton() {
         return singletonBean.hashCode();
@@ -26,13 +39,12 @@ public class BeanController {
 
     @GetMapping("/prototype")
     public int getPrototype() {
-        PrototypeBean prototypeBean = applicationContext.getBean(PrototypeBean.class);
+        PrototypeBean prototypeBean = prototypeBeanObjectProvider.getObject();
         return prototypeBean.hashCode();
     }
 
     @GetMapping("/request")
     public int getRequest(RequestBean requestBean) {
-//        RequestBean requestBean = applicationContext.getBean(RequestBean.class);
         return requestBean.hashCode();
     }
 
